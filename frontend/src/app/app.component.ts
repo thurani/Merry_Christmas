bae21a4e3e332a17b54592e2b66ebf49ae09ac7b
thurani <tcsenanayake@gmail.com>
2024-11-20 20:22:44 +0000

    Initial Commit

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+  title = 'Merry Christmas';
+
+  newWish = { name: '', email: '', description: '' };
+  wishes:any = []
+
+  APIURL = "http://localhost:8000/";
+
+  constructor(private http:HttpClient){}
+
+  selectedDiv: string = '';
+
+  ngOnInit(){
+    this.get_wishes();
+  }
+
+  showDiv(divId: string) {
+    this.selectedDiv = divId;
+  }
+
+  get_wishes(){
+    this.http.get(this.APIURL + "get_wishes/").subscribe((res)=>{
+      this.wishes = res;
+    })
+  }
+
+  add_wishes(){
+    // const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    if (this.newWish.name && this.newWish.email && this.newWish.description){
+      this.http.post(this.APIURL + "add_wishes/", this.newWish).subscribe({
+        next: (response) => {
+          console.log('Wish added:', response);
+          this.get_wishes();
+          this.newWish = { name: '', email: '', description: '' };
+        },
+        error: (error) => {
+          console.error('Error adding wish:', error);
+        }
+    })
+    }
+  }
+}
